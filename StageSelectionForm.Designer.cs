bb5cc859cbd50cf64d420d574a601e4eeed463f5
jupahe64 <33004544+jupahe64@users.noreply.github.com>
2018-09-03 23:28:13 +0200

    moving treeview generation for StageSelectionForm from designer to code
    kingdoms done:
    cap, cascade, sand, lake

--- StageSelectionForm.Designer.cs
+++ StageSelectionForm.Designer.cs
@@ -28,39 +28,6 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Cap Kingdom");
-			System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Cascade Kingdom");
-			System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("MoEye moving Platform");
-			System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Challenge Subrooms", new System.Windows.Forms.TreeNode[] {
-            treeNode3});
-			System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Pyramid(Starting Area)");
-			System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Pyramid(Bullet Bill Parkour)");
-			System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Ice Underground before Boss");
-			System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Ice Underground Boss");
-			System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Sand Kindom", new System.Windows.Forms.TreeNode[] {
-            treeNode4,
-            treeNode5,
-            treeNode6,
-            treeNode7,
-            treeNode8});
-			System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("LakeKingdom (Town Area)");
-			System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Lake Kingdom(Starting Area)", new System.Windows.Forms.TreeNode[] {
-            treeNode10});
-			System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Wodded Kingdom");
-			System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Cloud Kingdom(1. Bowser Fight)");
-			System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Lost Kingdom");
-			System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Theater (smb 1-1)");
-			System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Metro Kingdom", new System.Windows.Forms.TreeNode[] {
-            treeNode15});
-			System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Snow Kingdom");
-			System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Seaside Kingdom");
-			System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Luncheon Kingdom");
-			System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("Ruined Kingdom");
-			System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("Bowser\'s Kingdom");
-			System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("Moon Kingdom");
-			System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("Mushroom Kingdom");
-			System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("Dark Side of the Moon");
-			System.Windows.Forms.TreeNode treeNode25 = new System.Windows.Forms.TreeNode("Darker Side of the Moon");
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
 			this.treeView1 = new System.Windows.Forms.TreeView();
@@ -95,98 +62,6 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.treeView1.Location = new System.Drawing.Point(1, 1);
 			this.treeView1.Name = "treeView1";
-			treeNode1.Name = "Node0";
-			treeNode1.Tag = "CapWorldHomeStage";
-			treeNode1.Text = "Cap Kingdom";
-			treeNode2.Name = "Node1";
-			treeNode2.Tag = "WaterfallWorldHomeStage";
-			treeNode2.Text = "Cascade Kingdom";
-			treeNode3.Name = "Node4";
-			treeNode3.Tag = "MeganeLiftExStage";
-			treeNode3.Text = "MoEye moving Platform";
-			treeNode4.Name = "Node3";
-			treeNode4.Text = "Challenge Subrooms";
-			treeNode5.Name = "Node7";
-			treeNode5.Tag = "SandWorldPyramid000Stage";
-			treeNode5.Text = "Pyramid(Starting Area)";
-			treeNode6.Name = "Node8";
-			treeNode6.Tag = "SandWorldPyramid001Stage";
-			treeNode6.Text = "Pyramid(Bullet Bill Parkour)";
-			treeNode7.Name = "Node9";
-			treeNode7.Tag = "SandWorldUnderground000Stage";
-			treeNode7.Text = "Ice Underground before Boss";
-			treeNode8.Name = "Node10";
-			treeNode8.Tag = "SandWorldUnderground001Stage";
-			treeNode8.Text = "Ice Underground Boss";
-			treeNode9.Name = "Node2";
-			treeNode9.Tag = "SandWorldHomeStage";
-			treeNode9.Text = "Sand Kindom";
-			treeNode10.Name = "Node0";
-			treeNode10.Tag = "LakeWorldTownZone";
-			treeNode10.Text = "LakeKingdom (Town Area)";
-			treeNode11.Name = "Node3";
-			treeNode11.Tag = "LakeWorldHomeStage";
-			treeNode11.Text = "Lake Kingdom(Starting Area)";
-			treeNode12.Name = "Node4";
-			treeNode12.Tag = "ForestWorldHomeStage";
-			treeNode12.Text = "Wodded Kingdom";
-			treeNode13.Name = "Node5";
-			treeNode13.Tag = "DemoCrashHomeFallStage";
-			treeNode13.Text = "Cloud Kingdom(1. Bowser Fight)";
-			treeNode14.Name = "Node6";
-			treeNode14.Tag = "ClashWorldHomeStage";
-			treeNode14.Text = "Lost Kingdom";
-			treeNode15.Name = "Node1";
-			treeNode15.Tag = "Theater2DExStage";
-			treeNode15.Text = "Theater (smb 1-1)";
-			treeNode16.Name = "Node7";
-			treeNode16.Tag = "CityWorldHomeStage";
-			treeNode16.Text = "Metro Kingdom";
-			treeNode17.Name = "Node8";
-			treeNode17.Tag = "SnowWorldHomeStage";
-			treeNode17.Text = "Snow Kingdom";
-			treeNode18.Name = "Node9";
-			treeNode18.Tag = "SeaWorldHomeStage";
-			treeNode18.Text = "Seaside Kingdom";
-			treeNode19.Name = "Node10";
-			treeNode19.Tag = "LavaWorldHomeStage";
-			treeNode19.Text = "Luncheon Kingdom";
-			treeNode20.Name = "Node11";
-			treeNode20.Tag = "BossRaidWorldHomeStage";
-			treeNode20.Text = "Ruined Kingdom";
-			treeNode21.Name = "Node12";
-			treeNode21.Tag = "SkyWorldHomeStage";
-			treeNode21.Text = "Bowser\'s Kingdom";
-			treeNode22.Name = "Node13";
-			treeNode22.Tag = "MoonWorldHomeStage";
-			treeNode22.Text = "Moon Kingdom";
-			treeNode23.Name = "Node14";
-			treeNode23.Tag = "PeachWorldHomeStage";
-			treeNode23.Text = "Mushroom Kingdom";
-			treeNode24.Name = "Node15";
-			treeNode24.Tag = "Special1WorldHomeStage";
-			treeNode24.Text = "Dark Side of the Moon";
-			treeNode25.Name = "Node16";
-			treeNode25.Tag = "Special2WorldHomeStage";
-			treeNode25.Text = "Darker Side of the Moon";
-			this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2,
-            treeNode9,
-            treeNode11,
-            treeNode12,
-            treeNode13,
-            treeNode14,
-            treeNode16,
-            treeNode17,
-            treeNode18,
-            treeNode19,
-            treeNode20,
-            treeNode21,
-            treeNode22,
-            treeNode23,
-            treeNode24,
-            treeNode25});
 			this.treeView1.Size = new System.Drawing.Size(345, 379);
 			this.treeView1.TabIndex = 2;
 			this.treeView1.DoubleClick += new System.EventHandler(this.treeView1_DoubleClick);
--- StageSelectionForm.cs
+++ StageSelectionForm.cs
@@ -18,8 +18,84 @@ namespace OdysseyExt
 		public StageSelectionForm()
 		{
 			InitializeComponent();
+
+			#region treeview
+			treeView1.Nodes.Add(ContainerLevel("Cap Kingdom", "CapWorldHomeStage",
+				Level("Top-Hat Tower", "CapWorldTowerStage"),
+				Folder("Sub rooms",
+					Level("Poison Tides","PoisonWaveExStage"),
+					Level("Push-Blocks", "PushBlockExStage"),
+					Level("Frog Pound", "FrogSearchExStage"),
+					Level("Rolling Lane", "RollingExStage")
+				)
+			));
+			treeView1.Nodes.Add(ContainerLevel("Cascade Kingdom", "WaterfallWorldHomeStage",
+				Folder("Sub rooms",
+					Level("T-Rex Nest", "TrexPoppunExStage"),
+					Level("ChainChomp", "WanwanClashExStage"),
+					Level("Chasm Lifts", "Lift2DExStage"),
+					Level("Mysterious Clouds", "CapAppearExStage"),
+					Level("Gusty Bridges", "WindBlowExStage")
+				)
+			));
+			treeView1.Nodes.Add(ContainerLevel("Sand Kingdom", "SandWorldHomeStage",
+				Level("Town", "SandWorldHomeTownZone"),
+				Level("Round Tower", "SandWorldKillerTowerZone"),
+				Level("Shop", "SandWorldShopStage"),
+				Level("Slots", "SandWorldSlotStage"),
+				Level("Costume Needed: Dancing", "SandWorldCostumeStage"),
+				Level("Sphynx Treasure Vault", "SandWorldSecretStage"),
+				Level("Vibration Room", "SandWorldVibrationStage"),
+				Level("Inverted Pyramid: Lower Interior", "SandWorldPyramid000Stage"),
+				Level("Inverted Pyramid: Upper Interior", "SandWorldPyramid001Stage"),
+				Level("Cave below the Pyramid", "SandWorldUnderground000Stage"),
+				Level("KnuckleTec Boss Arena", "SandWorldUnderground001Stage"),
+				Folder("Sub rooms",
+					Level("Ice Cave", "SandWorldPressExStage"),
+					Level("Moe-Eye Invisible Maze", "SandWorldMeganeExStage"),
+					Level("Bullet Bill Maze", "SandWorldKillerExStage"),
+					Level("Jaxi Ruins", "SandWorldSphinxExStage"),
+					Level("Rotating Houses", "SandWorldRotateExStage"),
+					Level("Moe-Eye Invisible Floor", "MeganeLiftExStage"),
+					Level("Colossal Ruins", "RocketFlowerExStage"),
+					Level("Freezing Waterway", "WaterTubeExStage")
+				)
+			));
+			treeView1.Nodes.Add(ContainerLevel("Lake Kingdom", "LakeWorldHomeStage",
+				Level("Town", "LakeWorldTownZone"),
+				Level("8-bit Section", "LakeWorld2DZone"),
+				Level("Shop", "LakeWorldShopStage"),
+				Level("Timer Challenge 2", "LakeWorldTimerAthletic000Zone"),
+				Folder("Sub rooms",
+					Level("Statue Puzzle", "GotogotonExStage"),
+					Level("Zipper Chasm", "FastenerExStage"),
+					Level("Bouncy Flowers", "TrampolineWallCatchExStage"),
+					Level("Poison Swamp", "FrogPoisonExStage")
+				)
+			));
+			#endregion
 		}
-		
+
+		#region treeview
+		private static TreeNode Level(string name, string internalName)
+		{
+			var tn = new TreeNode(name);
+			tn.Tag = internalName;
+			return tn;
+		}
+		private static TreeNode Folder(string name, params TreeNode[] children)
+		{
+			var tn = new TreeNode(name, children);
+			return tn;
+		}
+		private static TreeNode ContainerLevel(string name, string internalName, params TreeNode[] children)
+		{
+			var tn = new TreeNode(name,children);
+			tn.Tag = internalName;
+			return tn;
+		}
+		#endregion
+
 		private void button2_Click(object sender, EventArgs e)
 		{
 			Result = null;
